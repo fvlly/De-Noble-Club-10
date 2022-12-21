5ea389b300e548f824b2f0e3ae08f50112b384ab
Fvlly <luki0000001@gmail.com>
2022-12-21 12:37:56 +0100

    add 404page

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,40 @@ import Header from "./components/Header";
 import Home from "./components/pages/Home";
 import Footer from "./components/Footer";
 import ErrorFallback from "./components/ErrorBoundary";
+import NotFound from "./components/pages/NotFound";
 import { Box } from "@chakra-ui/react";
 
 const lazyAbout = lazy(() => import("./components/pages/About"));
 const lazyProjects = lazy(() => import("./components/pages/Projects"));
 const lazyAwareness = lazy(() => import("./components/pages/Awareness"));
 const lazyContact = lazy(() => import("./components/pages/Contact"));
-const lazyMembership = lazy(()=> import("./components/pages/Membership") )
+const lazyMembership = lazy(() => import("./components/pages/Membership"));
 
 const App = () => {
   return (
     <Router>
-      <ErrorBoundary FallbackComponent={ErrorFallback} onReset={()=>{
-        window.location.reload()
-      }} >
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => {
+          window.location.reload();
+        }}
+      >
         <Header />
-        <Suspense fallback={<Box bgColor='gray.900' minH='100vh'> loading ...</Box>}>
+        <Suspense
+          fallback={
+            <Box bgColor="gray.900" minH="100vh">
+              {" "}
+              loading ...
+            </Box>
+          }
+        >
           <Route exact path="/" component={Home} />
           <Route path="/about" component={lazyAbout} />
           <Route path="/projects" component={lazyProjects} />
           <Route path="/awareness" component={lazyAwareness} />
           <Route path="/membership" component={lazyMembership} />
           <Route path="/contact" component={lazyContact} />
+          <Route path="*" component={NotFound} />
         </Suspense>
         <Footer />
       </ErrorBoundary>
---
+++ src/components/pages/NotFound.jsx
@@ -0,0 +1,22 @@
+import { Box, Flex, Heading, Stack, StackDivider, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <Flex as="section" bgColor="gray.900" minH='100vh' justify='center' align='center'>
+      <Box>
+        <Heading color='red.600' py='4'>Oops! You seem to be lost.</Heading>
+        <Text>Here are some helpful links:</Text>
+        <Stack direction={['column','column','row']} spacing='2' divider={<StackDivider borderColor='gray.200' />} >
+        <Link to="/">Home</Link >
+        <Link to="/about">About</Link>
+        <Link to="/awareness">Awareness</Link>
+        <Link to="/project">Project</Link>
+        <Link to="/contact">Contact</Link>
+        </Stack>
+      </Box>
+    </Flex>
+  );
+};
+
+export default NotFound;
